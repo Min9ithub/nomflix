ae6ffa3102911cffbebed531c998388dbc781667
Nam Min Ki <nam960216@gmail.com>
2022-12-29 21:42:30 +0900

    modify gh-pages

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, Link } from "react-router-dom";
 import ErrorComponent from "./Components/ErrorComponent";
 import NotFound from "./NotFound";
 import Root from "./Root";
@@ -6,32 +6,35 @@ import Home from "./Routes/Home";
 import Search from "./Routes/Search";
 import Tv from "./Routes/Tv";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Root />,
-    children: [
-      {
-        path: "",
-        element: <Home />,
-        errorElement: <ErrorComponent />,
-      },
-      //   /movie/:id router를 중첩라우팅 하는 방법 찾기
-      {
-        path: "movies/:id",
-        element: <Home />,
-      },
-      {
-        path: "tv",
-        element: <Tv />,
-      },
-      {
-        path: "search",
-        element: <Search />,
-      },
-    ],
-    errorElement: <NotFound />,
-  },
-]);
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <Root />,
+      children: [
+        {
+          path: "",
+          element: <Home />,
+          errorElement: <ErrorComponent />,
+        },
+        //   /movie/:id router를 중첩라우팅 하는 방법 찾기
+        {
+          path: "movies/:id",
+          element: <Home />,
+        },
+        {
+          path: "tv",
+          element: <Tv />,
+        },
+        {
+          path: "search",
+          element: <Search />,
+        },
+      ],
+      errorElement: <NotFound />,
+    },
+  ],
+  { basename: "/" }
+);
 
 export default router;
